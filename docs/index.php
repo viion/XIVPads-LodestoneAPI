f98aac40c934beacb84715f13c63de243a5c70fb
Josh Freeman <josh@viion.co.uk>
2014-07-14 10:59:06 +0100

    Initial stages of the APIs documentation

---
+++ docs/index.php
@@ -0,0 +1,63 @@
+<!doctype html>
+<html class="xivpads">
+    <head>
+
+    <!-- Default stuff -->
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta name="viewport" content="width=device-width, maximum-scale=1">
+    <title>Viion - FFXIV Lodestone API (GitHub)</title>
+
+    <!-- CDN -->
+    <link href='http://fonts.googleapis.com/css?family=Roboto:400,100,300,700,900,500italic,500,700italic' rel='stylesheet' type='text/css'>
+    <link href='http://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.1.0/css/font-awesome.min.css' rel='stylesheet' type='text/css'>
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <script src="http://cdnjs.cloudflare.com/ajax/libs/mustache.js/0.8.1/mustache.js"></script>
+
+    <!-- Custom -->
+    <link href='css/main.css' rel='stylesheet' type='text/css'>
+
+    </head>
+    <body>
+
+        <header>
+            <h1>FFXIV Lodestone API</h1>
+            <h2>Documentation &nbsp;-&nbsp; <a href="https://github.com/viion/XIVPads-LodestoneAPI">GitHub: Viion "XIVPads-LodestoneAPI"</a></h2>
+        </header>
+
+        <nav> 
+            <div class="box">
+                <h3>Classes</h3>
+                <a href="">API</a>
+                <a href="">Lodestone</a>
+                <a href="">Social</a>
+                <a href="">Character</a>
+                <a href="">FreeCompany</a>
+                <a href="">Linkshell</a>
+                <a href="">Achievements</a>
+                <a href="">Parser</a>
+            </div>
+
+            <div class="box">
+                <h3>Traits</h3>
+                <a href="" class="green">Funky</a>
+                <a href="" class="green">Config</a>
+            </div>
+
+            <div class="box">
+                <h3>Helpers</h3>
+                <a href="" class="orange">Logger</a>
+            </div>
+        </nav>
+
+        <main>
+            Site Contents
+        </main>
+
+        <footer>
+            Lodestone API by @Viion
+        </footer>
+
+    </body>
+</html>
