db08eed42c17bcdc9854d96ccd9ce96f6980a054
Josh Freeman <josh@viion.co.uk>
2015-02-26 23:26:16 +0000

    Update test to include memory info and allow ?id=num

--- test.php
+++ test.php
@@ -1,21 +1,32 @@
 <?php
+
+
 $start = microtime(true);
+show("Memory: ". cMem(memory_get_usage()) .' - start');
 
 // debug function
 function show($data) { echo '<pre>'. print_r($data, true) .'</pre>'; }
+function cMem($size) { return @round($size/pow(1024,($i=floor(log($size,1024)))),2).' '.array('b','kb','mb','gb','tb','pb')[$i]; }
 
 // require auto loader
 //require 'vendor/autoload.php';
+show("Memory: ". cMem(memory_get_usage()) .' - before autoloader');
 require 'api-autoloader.php';
 use Viion\Lodestone\LodestoneAPI;
+show("Memory: ". cMem(memory_get_usage()) .' - after autoloader');
 
 // new API
 $api = new LodestoneAPI();
+show("Memory: ". cMem(memory_get_usage()) .' - after new api instance');
 
 // Get character
 
 //    $character = $api->Search->Character('Premium Virtue', 'Hyperion');
-    $character = $api->Search->Character(730968);
+    $id = 730968;
+    if (isset($_GET['id'])) {
+        $id = intval($_GET['id']);
+    }
+    $character = $api->Search->Character($id);
     show($character->dump());
 
     //$worldStatus = $api->Search->Worldstatus('Chaos','Zodiark');
@@ -26,7 +37,13 @@ $api = new LodestoneAPI();
 $achievements = $api->Search->Achievements(730968, true);
 show($achievements->dump());
 */
+show("Memory: ". cMem(memory_get_usage()) .' - after api->Search->Character');
 $finish = microtime(true);
 
+unset($api);
+show("Memory: ". cMem(memory_get_usage()) .' - unset API');
+
 show("Duration: ". ($finish - $start) .' ms');
-show("Memory: ". memory_get_peak_usage(true));
+show("Memory Peak: ". cMem(memory_get_peak_usage()));
+
+show("Memory: ". cMem(memory_get_usage()) .' - end');
