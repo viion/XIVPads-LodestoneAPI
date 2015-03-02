bd1cc229f7bb1b6484c0041f2cdb6d196d6b42fd
Josh Freeman <josh@viion.co.uk>
2015-03-02 12:08:51 +0000

    ocd

--- src/Funky.php
+++ src/Funky.php
@@ -45,11 +45,12 @@ trait Funky
 
         return $html;
     }
-	
-	public function clearRegExpArray(&$array){
+
+	public function clearRegExpArray(&$array)
+    {
 		$tmp = array();
-		foreach($array as $key => $value){
-			if(!is_numeric($key)){
+		foreach($array as $key => $value) {
+			if(!is_numeric($key) ){
 				$tmp[$key] = $value;
 			}
 		}
