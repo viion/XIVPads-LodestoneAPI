90450b386c9707f31361f0a3a20c021ca58931e5
FlyerUA <igor.yavych@gmail.com>
2015-03-14 15:27:31 +0200

    Update Character.php
    Added 64x64 avatar

--- src/Character.php
+++ src/Character.php
@@ -14,6 +14,7 @@ class Character
     public $title;
     public $avatar;
     public $avatarLarge;
+    public $avatar64;
     public $avatarHash;
     public $portrait;
     public $portraitLarge;
