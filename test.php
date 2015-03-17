a3b80b9b3961372a7a57e596b23d2ddfff350f78
Benjamin Schnittger <bschnittger@ennit.de>
2015-03-17 13:09:54 +0100

    no message

--- test.php
+++ test.php
@@ -54,6 +54,7 @@ else if (isset($_GET['search']))
     $data = $api->Search->Character(trim(urldecode($_GET['name'])), trim($_GET['server']));
 }
 else if (isset($_GET['news'])){
+	// $_GET['func'] should be Topics, Maintenance, Status, Updates or Notices
 	$func = isset($_GET['func']) ? $_GET['func'] : 'Topics';
 	$data = $api->Search->{$func}();
 	show($data);
