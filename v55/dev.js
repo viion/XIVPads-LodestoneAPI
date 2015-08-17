0eab2dbed763b11c76c7565a879d8975fd202476
Josh Freeman <josh@viion.co.uk>
2015-08-17 16:11:01 +0100

    Begin item parsing.

---
+++ v55/dev.js
@@ -0,0 +1,28 @@
+// - - - - - - - - - - - - - - - - - - - -
+// This dev file is just so I can get a
+// bunch of correct jquery stuff, copied
+// into their respective parsing scripts
+// - - - - - - - - - - - - - - - - - - - -
+
+// prevent issues
+function ldst_strftime(time, stamp) {}
+
+// get
+$.get('dev-html.html', function(data) {
+    $('.dev').html(data);
+    // - - - - - - - - - - - - - - - - - - - -
+
+
+    var $box = $('.item_detail_box');
+    var data =
+    {
+        name: $box.find('.item_name_area h2').text(),
+        color: $box.find('.item_name_area h2').attr('class').split(' ')[1],
+        category: $box.find('.item_name_area').clone().children().remove().end().text(),
+    }
+
+
+
+    console.log(data);
+
+});
