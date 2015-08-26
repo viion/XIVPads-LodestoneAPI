e2c13ecf4ade5c14dc138faa3ec174f11f844c33
Josh Freeman <josh@viion.co.uk>
2015-08-26 17:01:23 +0100

    Free company search results.

--- v55/dev.js
+++ v55/dev.js
@@ -22,10 +22,23 @@ $.get('dev-html.html', function(data) {
     $('.dev').html(data);
     // - - - - - - - - - - - - - - - - - - - -
 
+    var data = [];
     $('.table_elements_com_fc tr').each(function() {
         $node = $(this);
 
+        var freecompany = {
+            id: $node.find('.ic_freecompany_box a').attr('href').split('/')[3],
+            emblums: {
+                1: $('.ic_crest_64 img').eq(0).attr('src').trim(),
+                2: $('.ic_crest_64 img').eq(1).attr('src').trim(),
+                3: $('.ic_crest_64 img').eq(2).attr('src').trim(),
+            },
+            grand_company: $node.find('.player_name_area .groundcompany_name').text().trim(),
+            name: $node.find('.player_name_area h4 a').text().trim(),
+            server: $node.find('.player_name_area h4 span').text().replace('(', '').replace(')', '').trim(),
+        };
 
+        data.push(freecompany);
     });
 
     console.log(data);
