1a50149437fdf752bb1948f9ab9861b0d3cbdea6
Josh Freeman <josh@viion.co.uk>
2015-10-17 12:27:36 +0100

    Don't call reply.

--- app.js
+++ app.js
@@ -30,7 +30,6 @@ server.ext('onPreResponse', function(request, reply) {
     request.response.header('Content-Type', 'application/json');
     request.response.header('Access-Control-Allow-Origin', '*');
     request.response.header('Cache-Control', 'max-age=3600');
-    reply();
 });
 
 // - - - - - - - - - - - - - - - - - - - - - - - - -
