e17de067dee83f094cac993472bd9b0020caa301
Josh Freeman <Josh@viion.co.uk>
2016-02-07 18:55:23 +0000

    Try cache busting

--- api/api.js
+++ api/api.js
@@ -39,7 +39,7 @@ var api = {
         var options = {
             host: host,
             port: 80,
-            path: url.replace(' ', '+'),
+            path: url.replace(' ', '+') + '#' + Date.now(),
         }
 
         // get
