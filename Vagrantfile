671365db18ef077e372b779b3643b9a272e7faac
Josh Freeman <josh@viion.co.uk>
2016-05-19 16:41:48 +0100

    Setup vagrant

---
+++ Vagrantfile
@@ -0,0 +1,22 @@
+# -*- mode: ruby -*-
+# vi: set ft=ruby :
+
+Vagrant.configure(2) do |config|
+
+    config.vm.box = "ubuntu/trusty64"
+    config.vm.provision :shell, path: "vm/provision.sh"
+    config.vm.network "forwarded_port", guest: 80, host: 4545
+
+    config.vm.provider :virtualbox do |vb|
+        vb.customize ["modifyvm", :id, "--natdnshostresolver1", "on"]
+        vb.memory = 1024
+        vb.cpus = 2
+    end
+
+    config.vm.network "private_network", ip: "14.14.14.14"
+    config.vm.hostname = "xivsync.dev"
+    config.vm.synced_folder ".", "/vagrant", type: "nfs"
+
+    config.hostmanager.enabled = true
+    config.hostmanager.manage_host = true
+end
---
+++ vm/provision.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+cd /vagrant
+USER=vagrant
+sudo locale-gen en_GB.UTF-8
+
+# Settings
+mysqlPassword="xivsync"
+mysqlDbUser="xivsync"
+mysqlDbPass="xivsync"
+mysqlDbName="xivsync"
+
+# Set global mysql user/pass
+echo "mysql-server mysql-server/root_password password $mysqlPassword" | debconf-set-selections
+echo "mysql-server mysql-server/root_password_again password $mysqlPassword" | debconf-set-selections
+
+# Software
+sudo apt-get update
+sudo apt-get install -y curl mysql-server git htop zip
+
+# Mysql
+sudo mysql --defaults-file=/etc/mysql/debian.cnf -e "CREATE USER '$mysqlDbUser'@'localhost' IDENTIFIED BY '$mysqlDbPass';"
+sudo mysql --defaults-file=/etc/mysql/debian.cnf -e "CREATE DATABASE $mysqlDbName;"
+sudo mysql --defaults-file=/etc/mysql/debian.cnf -e "GRANT ALL PRIVILEGES ON *.* TO '$mysqlDbUser'@'localhost';"
+sudo mysql --defaults-file=/etc/mysql/debian.cnf -e "FLUSH PRIVILEGES;"
+
+# Install node
+sudo curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
+sudo apt-get -y install nodejs
