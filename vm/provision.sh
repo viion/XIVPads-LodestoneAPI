b0286e80247c746c24159d885f6142bd1803c63c
Josh Freeman <Josh@viion.co.uk>
2016-07-20 18:40:35 +0100

    Update database schema and import it during provisioning.

--- vm/provision.sh
+++ vm/provision.sh
@@ -2,6 +2,7 @@
 cd /vagrant
 USER=vagrant
 sudo locale-gen en_GB.UTF-8
+npm config set bin-links false
 
 # Settings
 mysqlPassword="xivsync"
@@ -22,6 +23,7 @@ sudo mysql --defaults-file=/etc/mysql/debian.cnf -e "CREATE USER '$mysqlDbUser'@
 sudo mysql --defaults-file=/etc/mysql/debian.cnf -e "CREATE DATABASE $mysqlDbName;"
 sudo mysql --defaults-file=/etc/mysql/debian.cnf -e "GRANT ALL PRIVILEGES ON *.* TO '$mysqlDbUser'@'localhost';"
 sudo mysql --defaults-file=/etc/mysql/debian.cnf -e "FLUSH PRIVILEGES;"
+sudo mysql -u $mysqlDbUser -p$mysqlDbPass $mysqlDbPas < /home/vagrant/vm/database.sql
 
 # Install node
 sudo curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
@@ -29,6 +31,7 @@ sudo apt-get -y install nodejs
 
 # Map IP Table
 sudo iptables -t nat -A PREROUTING -p tcp --dport 80 -j REDIRECT --to-port 3000
+sudo iptables -t nat -A PREROUTING -p tcp --dport 80 -j REDIRECT --to-port 8080
 
 # Run node app
 npm install pm2 -g
